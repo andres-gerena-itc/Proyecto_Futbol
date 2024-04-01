a923bd1f4af6410a4293b34bf555530f1ad9f611
Fabian11037 <fesuareze@itc.edu.co>
2024-03-31 23:20:11 -0500

    Agregación Funciones JS
    Se agregaron 3 scripts de JS

--- assets/js/mundial/main.js
+++ assets/js/mundial/main.js
@@ -9,3 +9,25 @@ formulario.addEventListener("submit", (e) => {
     const email = document.getElementById("email");
     email.value = "";
 });
+
+//Suma de 0 a n
+function sumar() {
+    let numero = parseInt(document.getElementById("numero").value);
+    let suma = 0;
+    for (let i = 0; i <= numero; i++) {
+        suma += i;
+    }
+    document.getElementById("resultado").textContent = `La suma de 0 hasta ${numero} es: ${suma}`;
+}
+
+// Modificar un texto dependiendo de la hora
+var itemOne = document.getElementById("cambio")
+var today = new Date();
+let hora = today.getHours();
+let mes = today.getMonth();
+if (hora >= 12) {
+    itemOne.textContent = "MOMENTOS DESTACABLES";
+}
+else if (hora <12) {
+    itemOne.textContent = "MOMENTOS MEMORABLES";
+}
