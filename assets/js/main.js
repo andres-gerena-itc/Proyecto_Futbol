0b6fcc64b3b6c6c9530adf8c49b9267c7fe582a0
Andres-Gerena-ITC <afgerenac@itc.edu.co>
2024-03-31 21:04:40 -0500

    Se agregó js funcional
    Se integró Javascript funcional para

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,27 @@ formulario.addEventListener("submit", (e) => {
     const email = document.getElementById("email");
     email.value = "";
 });
+
+
+//Suma de 0 a n
+
+function sumar() {
+    let numero = parseInt(document.getElementById("numero").value);
+    let suma = 0;
+    for (let i = 0; i <= numero; i++) {
+        suma += i;
+    }
+
+    document.getElementById("resultado").textContent = `La suma de 0 hasta ${numero} es: ${suma}`;
+}
+
+
+// Se genera un While, ese indicará cuales años han habido copas del mundo
+
+let contador = 1930;
+
+while (contador <= 2022) {
+    const elementoBloque = document.getElementById("copas-mundiales")
+    elementoBloque.innerHTML += contador + " ";
+    contador += 4;
+}
